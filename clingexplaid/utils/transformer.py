c30ac28c557af449c9bdeeea56eae603cd0cea41
Hannes Weichelt <hweichelt@uni-potsdam.de>
2023-06-15 19:49:02 +0200

    added legacy functions to RuleIDTransformer

--- clingexplaid/utils/transformer.py
+++ clingexplaid/utils/transformer.py
@@ -55,7 +55,7 @@ class RuleIDTransformer(Transformer):
         node.body.insert(len(node.body), symbol)
         return node.update(**self.visit_children(node))
 
-    def get_transformed(self, program_string: str) -> TransformerResult:
+    def get_transformed_string(self, program_string: str) -> str:
         """
         Function that applies the transformation on the `program_string` it's called with and returns a
         TransformerResult with the transformed program-string and the necessary assumptions for the
@@ -66,14 +66,19 @@ class RuleIDTransformer(Transformer):
         parse_string(program_string, lambda stm: out.append((str(self(stm)))))
         out.append(f"{{_rule(1..{self.rule_id})}} % Choice rule to allow all _rule atoms to become assumptions")
 
+        return "\n".join(out)
+
+    def get_transformed_assumptions(self, n_rules: Optional[int] = None) -> List[Tuple[clingo.Symbol, bool]]:
+        if n_rules is None:
+            n_rules = self.rule_id
+        return [(clingo.parse_term(f"{self.rule_id_signature}({rule_id})"), True) for rule_id in range(1, n_rules)]
+
+    def get_transformer_result(self, program_string: str) -> TransformerResult:
         result = TransformerResult(
-            output_string="\n".join(out),
-            output_assumptions=self._get_assumptions(self.rule_id)
+            output_string=self.get_transformed_string(program_string=program_string),
+            output_assumptions=self.get_transformed_assumptions()
         )
         return result
-
-    def _get_assumptions(self, n_rules) -> List[Tuple[clingo.Symbol, bool]]:
-        return [(clingo.parse_term(f"{self.rule_id_signature}({rule_id})"), True) for rule_id in range(1, n_rules)]
 
 
 class SignatureToAssumptionTransformer(Transformer):
@@ -127,7 +132,7 @@ bike(1); snake(1) :- something_true.
 
 
 rt = RuleIDTransformer()
-res = rt.get_transformed(prg)
+res = rt.get_transformer_result(prg)
 print(res.output_string)
 print(res.output_assumptions)
 
