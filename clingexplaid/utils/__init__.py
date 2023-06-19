ed347d96b2d9678c634f84e0267d7027f61f0f66
Hannes Weichelt <hweichelt@uni-potsdam.de>
2023-06-19 14:05:10 +0200

    added function for literal lookup

--- clingexplaid/utils/__init__.py
+++ clingexplaid/utils/__init__.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass
+
+import clingo
 from clingo.ast import ASTType
-from typing import Tuple
+from typing import Tuple, List, Dict
 
 
 def match_ast_symbolic_atom_signature(ast_symbol:ASTType.SymbolicAtom, signature: Tuple[str, int]):
@@ -23,3 +25,14 @@ def match_ast_symbolic_atom_signature(ast_symbol:ASTType.SymbolicAtom, signature
             arity += 1
 
     return all((signature[0] == name, signature[1] == arity))
+
+
+def get_solver_literal_lookup(control: clingo.Control) -> Dict[int, clingo.Symbol]:
+    """
+    This function can be used to get a lookup dictionary to associate literal ids with their respective symbols for all
+    symbolic atoms of the program
+    """
+    lookup = dict()
+    for atom in control.symbolic_atoms:
+        lookup[atom.literal] = atom.symbol
+    return lookup
