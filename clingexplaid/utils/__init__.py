61b2b1b050d40497a737f7a310dd5895bef0b0cf
Hannes Weichelt <hweichelt@uni-potsdam.de>
2023-06-26 13:57:43 +0200

    improved implementation of arity calculation for ast SymbolicAtom match

--- clingexplaid/utils/__init__.py
+++ clingexplaid/utils/__init__.py
@@ -3,28 +3,15 @@ from clingo.ast import ASTType
 from typing import Tuple, Dict
 
 
-def match_ast_symbolic_atom_signature(ast_symbol:ASTType.SymbolicAtom, signature: Tuple[str, int]):
+def match_ast_symbolic_atom_signature(ast_symbol: ASTType.SymbolicAtom, signature: Tuple[str, int]):
     """
     Function to match the signature of an AST SymbolicAtom to a tuple containing a string and int value, representing a
     matching signature.
     """
+
     symbol = str(ast_symbol.symbol)
     name = symbol.split('(')[0]
-    arity = 0
-
-    depth = 0
-    for c in symbol:
-        if c == "(":
-            if arity == 0:
-                arity = 1
-            depth += 1
-        elif c == ")":
-            depth -= 1
-
-        if depth != 1:
-            continue
-        if c == ",":
-            arity += 1
+    arity = len(ast_symbol.symbol.arguments)
 
     return all((signature[0] == name, signature[1] == arity))
 
