49f5d7f0d2bb70a8d919dd854d94c73601b6d1cc
Hannes Weichelt <hweichelt@uni-potsdam.de>
2024-01-15 12:57:09 +0100

    added FactTransformer to remove program facts

--- src/clingexplaid/utils/transformer.py
+++ src/clingexplaid/utils/transformer.py
@@ -11,6 +11,7 @@ from clingo import ast as _ast
 from clingexplaid.utils import match_ast_symbolic_atom_signature
 
 RULE_ID_SIGNATURE = "_rule"
+REMOVED_TOKEN = "__REMOVED__"
 
 
 class UntransformedException(Exception):
@@ -189,6 +190,69 @@ class AssumptionTransformer(_ast.Transformer):
             for sym in fact_symbols
             if sym in symbol_to_literal_lookup
         }
+
+
+class FactTransformer(_ast.Transformer):
+    """
+    CLASS DOC COMMENT
+    """
+
+    def __init__(self, signatures: Optional[Set[Tuple[str, int]]] = None):
+        self.signatures = signatures if signatures is not None else set()
+
+    def visit_Rule(self, node):  # pylint: disable=C0103
+        """
+        Removes all facts from a program that match the given signatures (if none are given all facts are removed).
+        """
+        if node.head.ast_type != _ast.ASTType.Literal:
+            return node
+        if node.body:
+            return node
+        has_matching_signature = any(
+            match_ast_symbolic_atom_signature(node.head.atom, (name, arity))
+            for (name, arity) in self.signatures
+        )
+        # if signatures are defined only transform facts that match them, else transform all facts
+        if self.signatures and not has_matching_signature:
+            return node
+
+        return _ast.Rule(
+            location=node.location,
+            head=_ast.Function(
+                location=node.location, name=REMOVED_TOKEN, arguments=[], external=0
+            ),
+            body=[],
+        )
+
+    @staticmethod
+    def post_transform(program_string: str) -> str:
+        # remove the transformed REMOVED_TOKENS from the resulting program string
+        rules = program_string.split("\n")
+        out = []
+        for rule in rules:
+            if not rule.startswith(REMOVED_TOKEN):
+                out.append(rule)
+        return "\n".join(out)
+
+    def parse_string(self, string: str) -> str:
+        """
+        Function that applies the transformation to the `program_string` it's called with and returns the transformed
+        program string.
+        """
+        out = []
+        _ast.parse_string(string, lambda stm: out.append(str(self(stm))))
+        return self.post_transform("\n".join(out))
+
+    def parse_files(self, paths: Sequence[Union[str, Path]]) -> str:
+        """
+        Parses the files and returns a string with the transformed program.
+        """
+        out = []
+        _ast.parse_files(
+            [str(p) for p in paths],
+            lambda stm: out.append(str(self(stm))),
+        )
+        return self.post_transform("\n".join(out))
 
 
 class ConstraintTransformer(_ast.Transformer):
