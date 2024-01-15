01a184b51689c9fcdec2b85858d3a8a9225275a5
Hannes Weichelt <hweichelt@uni-potsdam.de>
2024-01-15 13:27:06 +0100

    added util function to get model/string signatures

--- src/clingexplaid/utils/__init__.py
+++ src/clingexplaid/utils/__init__.py
@@ -2,6 +2,7 @@
 Utilities
 """
 
+import re
 from typing import Dict, Iterable, Set, Tuple, Union
 
 import clingo
@@ -44,3 +45,32 @@ __all__ = [
     match_ast_symbolic_atom_signature.__name__,
     get_solver_literal_lookup.__name__,
 ]
+
+
+def get_signatures_from_model_string(model_string: str) -> Dict[str, int]:
+    """
+    This function returns a dictionary of the signatures/arities of all atoms of a model string. Model strings are of
+    the form: `"signature1(X1, ..., XN) ... signatureM(X1, ..., XK)"`
+    """
+    signatures = {}
+    for atom_string in model_string.split():
+        result = re.search(r"([^(]*)\(", atom_string)
+        if len(result.groups()) == 0:
+            continue
+        signature = result.group(1)
+        # calculate arity for the signature
+        arity = 0
+        level = 0
+        for c in atom_string.removeprefix(signature):
+            if c == "(":
+                level += 1
+            elif c == ")":
+                level -= 1
+            else:
+                if level == 1 and c == ",":
+                    arity += 1
+        # if arity is not 0 increase by 1 for the last remaining parameter that is not followed by a comma
+        if arity > 0:
+            arity += 1
+        signatures[signature] = arity
+    return signatures
