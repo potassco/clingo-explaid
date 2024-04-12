e688cdda09541404ee2ff7caec0438b690ee0d82
Susana Hahn <susuhahnml@yahoo.com.mx>
2024-04-12 08:59:01 -0600

    Versions ci

--- noxfile.py
+++ noxfile.py
@@ -7,7 +7,7 @@ nox.options.sessions = "lint_pylint", "typecheck", "test"
 EDITABLE_TESTS = True
 PYTHON_VERSIONS = None
 if "GITHUB_ACTIONS" in os.environ:
-    PYTHON_VERSIONS = ["3.11"]
+    PYTHON_VERSIONS = ["3.12", "3.11"]
     EDITABLE_TESTS = False
 
 
