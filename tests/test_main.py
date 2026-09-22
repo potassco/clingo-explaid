7abdd85b8b7c5890cdd310bb63e25e165ada15d8
Hannes Weichelt <hweichelt@uni-potsdam.de>
2026-09-22 15:38:32 +0200

    Disabled logger & parser tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,12 +2,7 @@
 Test cases for main application functionality.
 """
 
-from io import StringIO
 from unittest import TestCase
-
-from clingexplaid.utils import logging
-from clingexplaid.utils.logging import configure_logging, get_logger
-from clingexplaid.utils.parser import get_parser
 
 
 class TestMain(TestCase):
@@ -15,20 +10,20 @@ class TestMain(TestCase):
     Test cases for main application functionality.
     """
 
-    def test_logger(self) -> None:
-        """
-        Test the logger.
-        """
-        sio = StringIO()
-        configure_logging(sio, logging.INFO, True)
-        log = get_logger("main")
-        log.info("test123")
-        self.assertRegex(sio.getvalue(), "test123")
+    # def test_logger(self) -> None:
+    #     """
+    #     Test the logger.
+    #     """
+    #     sio = StringIO()
+    #     configure_logging(sio, logging.INFO, True)
+    #     log = get_logger("main")
+    #     log.info("test123")
+    #     self.assertRegex(sio.getvalue(), "test123")
 
-    def test_parser(self) -> None:
-        """
-        Test the parser.
-        """
-        parser = get_parser()
-        ret = parser.parse_args(["--log", "info"])
-        self.assertEqual(ret.log, logging.INFO)
+    # def test_parser(self) -> None:
+    #     """
+    #     Test the parser.
+    #     """
+    #     parser = get_parser()
+    #     ret = parser.parse_args(["--log", "info"])
+    #     self.assertEqual(ret.log, logging.INFO)
